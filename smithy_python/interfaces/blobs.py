c1ecb4967f4ff4e82d90fcfef900aeea57f2c00d
JordonPhillips <JordonPhillips@users.noreply.github.com>
2021-09-28 14:54:03 +0200

    Add streaming blob interfaces

---
+++ smithy_python/interfaces/blobs.py
@@ -0,0 +1,39 @@
+from typing import Union, Protocol, runtime_checkable
+
+
+@runtime_checkable
+class ByteStream(Protocol):
+    """A file-like object with a read method that returns bytes."""
+
+    def read(self, size: int = -1) -> bytes:
+        pass
+
+
+@runtime_checkable
+class SeekableByteStream(ByteStream, Protocol):
+    """A file-like object with read, seek, and tell methods."""
+
+    def seek(self, offset: int, whence: int = 0) -> int:
+        pass
+
+    def tell() -> int:
+        pass
+
+
+@runtime_checkable
+class AsyncByteStream(Protocol):
+    """A file-like object with an async read method."""
+
+    async def read(self, size: int = -1) -> bytes:
+        pass
+
+
+# A union of all acceptable streaming blob types. Deserialized payloads will
+# always return a ByteStream, or AsyncByteStream if async is enabled.
+StreamingBlob = Union[
+    ByteStream,
+    SeekableByteStream,
+    AsyncByteStream,
+    bytes,
+    bytearray,
+]
