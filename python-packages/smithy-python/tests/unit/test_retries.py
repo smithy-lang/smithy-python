395147777003d8a21134d7682fc6d0a3648cdc74
Jonas Neubert <jonasneu@amazon.com>
2022-11-25 17:35:27 -0700

    exponential retry and simple retry strategy implementation

---
+++ python-packages/smithy-python/tests/unit/test_retries.py
@@ -0,0 +1,67 @@
+# Copyright 2022 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License"). You
+# may not use this file except in compliance with the License. A copy of
+# the License is located at
+#
+# http://aws.amazon.com/apache2.0/
+#
+# or in the "license" file accompanying this file. This file is
+# distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF
+# ANY KIND, either express or implied. See the License for the specific
+# language governing permissions and limitations under the License.
+
+from datetime import timedelta
+
+import pytest
+
+from smithy_python._private.retries import (
+    ExponentialRetryBackoffStrategy,
+    ExponentialBackoffJitterType as EBJT,
+)
+
+
+@pytest.mark.parametrize(
+    "jitter_type, scale_value, max_backoff, expected_delays",
+    [
+        # no jitter
+        (EBJT.NONE, 2, 20, [2.0, 4.0, 8.0, 16.0, 20.0, 20.0]),
+        (EBJT.NONE, 2.0, 20.0, [2.0, 4.0, 8.0, 16.0, 20.0, 20.0]),
+        (EBJT.NONE, 1.0, 20.0, [1.0, 2.0, 4.0, 8.0, 16.0, 20.0]),
+        (EBJT.NONE, 4.0, 2.0, [2.0, 2.0, 2.0]),
+        (EBJT.NONE, 23.4, 76.5, [23.4, 46.8, 76.5, 76.5]),
+        # full jitter
+        (EBJT.FULL, 2.0, 20.0, [1.0, 2.0, 4.0, 8.0, 10.0, 10.0]),
+        (EBJT.FULL, 5.0, 20.0, [2.5, 5.0, 10.0, 10.0]),
+        (EBJT.FULL, 5.0, 10.0, [2.5, 5.0, 5.0, 5.0]),
+        (EBJT.FULL, 23.4, 76.5, [11.7, 23.4, 38.25, 38.25]),
+        # equal jitter
+        (EBJT.DEFAULT, 2.0, 20.0, [1.5, 3.0, 6.0, 12.0, 15.0, 15.0]),
+        (EBJT.DEFAULT, 23.4, 76.5, [17.55, 35.1, 57.375, 57.375]),
+        # decorrelated jitter
+        (EBJT.DECORRELATED, 2.0, 20.0, [5.0, 9.5, 16.25, 20.0, 20.0]),
+        (EBJT.DECORRELATED, 23.4, 76.5, [58.5, 76.5, 76.5]),
+        # edge cases with zeros
+        (EBJT.NONE, 5.0, 0.0, [0.0, 0.0, 0.0]),
+        (EBJT.NONE, 0.0, 5.0, [0.0, 0.0, 0.0]),
+        (EBJT.NONE, 0.0, 0.0, [0.0, 0.0, 0.0]),
+        (EBJT.FULL, 5.0, 0.0, [0.0, 0.0, 0.0]),
+        (EBJT.FULL, 0.0, 5.0, [0.0, 0.0, 0.0]),
+        (EBJT.FULL, 0.0, 0.0, [0.0, 0.0, 0.0]),
+    ],
+)
+def test_exponential_backoff_strategy(
+    jitter_type, scale_value, max_backoff, expected_delays
+):
+    bos = ExponentialRetryBackoffStrategy(
+        backoff_scale_value=timedelta(seconds=scale_value),
+        max_backoff=timedelta(seconds=max_backoff),
+        jitter_type=jitter_type,
+        random=lambda: 0.5,  # every generated "random" value equals 0.5
+    )
+
+    for delay_index, delay_expected in enumerate(expected_delays):
+        delay_actual = bos.compute_next_backoff_delay(retry_attempt=delay_index)
+        delay_expected2 = timedelta(seconds=delay_expected)
+        print(f"{delay_index=} {delay_actual=} {delay_expected2=}")
+        assert delay_actual == timedelta(seconds=delay_expected)
