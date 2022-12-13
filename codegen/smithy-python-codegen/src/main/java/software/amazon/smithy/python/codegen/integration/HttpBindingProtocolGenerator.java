603a54b5c75decf3ad32c6bb7286d9ea332e4ef4
JordonPhillips <JordonPhillips@users.noreply.github.com>
2022-12-13 16:28:19 +0100

    Generate operation serde stubs for restJson

--- codegen/smithy-python-codegen/src/main/java/software/amazon/smithy/python/codegen/integration/HttpBindingProtocolGenerator.java
+++ codegen/smithy-python-codegen/src/main/java/software/amazon/smithy/python/codegen/integration/HttpBindingProtocolGenerator.java
@@ -15,7 +15,10 @@
 
 package software.amazon.smithy.python.codegen.integration;
 
+import software.amazon.smithy.model.knowledge.TopDownIndex;
+import software.amazon.smithy.model.shapes.OperationShape;
 import software.amazon.smithy.python.codegen.ApplicationProtocol;
+import software.amazon.smithy.python.codegen.CodegenUtils;
 import software.amazon.smithy.python.codegen.GenerationContext;
 import software.amazon.smithy.utils.SmithyUnstableApi;
 
@@ -36,10 +39,40 @@ public abstract class HttpBindingProtocolGenerator implements ProtocolGenerator 
     @Override
     public void generateRequestSerializers(GenerationContext context) {
         // TODO: Generate serializers for http bindings, e.g. non-body parts of the http request
+        var topDownIndex = TopDownIndex.of(context.model());
+        var delegator = context.writerDelegator();
+        var configSymbol = CodegenUtils.getConfigSymbol(context.settings());
+        var transportRequest = context.applicationProtocol().requestType();
+        for (OperationShape operation : topDownIndex.getContainedOperations(context.settings().getService())) {
+            var serFunction = getSerializationFunction(context, operation);
+            var input = context.model().expectShape(operation.getInputShape());
+            var inputSymbol = context.symbolProvider().toSymbol(input);
+            delegator.useFileWriter(serFunction.getDefinitionFile(), serFunction.getNamespace(), writer -> {
+                writer.write("""
+                    async def $L(input: $T, config: $T) -> $T:
+                        raise NotImplementedError()
+                    """, serFunction.getName(), inputSymbol, configSymbol, transportRequest);
+            });
+        }
     }
 
     @Override
     public void generateResponseDeserializers(GenerationContext context) {
         // TODO: Generate deserializers for http bindings, e.g. non-body parts of the http response
+        var topDownIndex = TopDownIndex.of(context.model());
+        var delegator = context.writerDelegator();
+        var configSymbol = CodegenUtils.getConfigSymbol(context.settings());
+        var transportResponse = context.applicationProtocol().responseType();
+        for (OperationShape operation : topDownIndex.getContainedOperations(context.settings().getService())) {
+            var deserFunction = getDeserializationFunction(context, operation);
+            var output = context.model().expectShape(operation.getOutputShape());
+            var outputSymbol = context.symbolProvider().toSymbol(output);
+            delegator.useFileWriter(deserFunction.getDefinitionFile(), deserFunction.getNamespace(), writer -> {
+                writer.write("""
+                    async def $L(http_response: $T, config: $T) -> $T:
+                        raise NotImplementedError()
+                    """, deserFunction.getName(), transportResponse, configSymbol, outputSymbol);
+            });
+        }
     }
 }
