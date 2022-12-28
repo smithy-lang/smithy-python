e1fc687df2cc5f8e4328dc3f49388f42c492b957
davidlm <davidlm@amazon.com>
2022-12-28 14:34:27 -0500

    pr feedback. moved auth stuff out of aws-smithy-python into smithy-python. Removed all usage of TypeVar and Generic and removed some files from gitignore.

--- python-packages/aws-smithy-python/aws_smithy_python/interfaces/identity.py
+++ python-packages/aws-smithy-python/aws_smithy_python/interfaces/identity.py
@@ -4,15 +4,18 @@
 # may not use this file except in compliance with the License. A copy of
 # the License is located at
 #
-#     http://aws.amazon.com/apache2.0/
+# http://aws.amazon.com/apache2.0/
 #
 # or in the "license" file accompanying this file. This file is
 # distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF
 # ANY KIND, either express or implied. See the License for the specific
 # language governing permissions and limitations under the License.
 
-from smithy_python.exceptions import SmithyException
+from smithy_python.interfaces.identity import Identity
 
 
-class AwsSmithyException(SmithyException):
-    """Base exception type for all exceptions raised by aws-smithy-python."""
+class AwsCredentialIdentity(Identity):
+
+    access_key_id: str
+    secret_key_id: str
+    session_token: str | None = None
--- python-packages/smithy-python/smithy_python/interfaces/identity.py
+++ python-packages/smithy-python/smithy_python/interfaces/identity.py
@@ -12,9 +12,7 @@
 # language governing permissions and limitations under the License.
 
 from datetime import datetime
-from typing import Any, Protocol, TypeVar
-
-from .http import Request
+from typing import Any, Protocol
 
 
 class Identity(Protocol):
@@ -23,93 +21,27 @@ class Identity(Protocol):
     expiration: datetime | None = None
 
 
-IdentityType = TypeVar("IdentityType", bound=Identity)
-
-
-class TokenIdentity(Identity):
-
-    token: str
-
-
-class LoginIdentity(Identity):
-
-    username: str
-    password: str
-
-
-class AnonymousIdentity(Identity):
-    ...
-
-
 class IdentityResolver(Protocol):
     """Used to load a user's `Identity` from a given source.
 
-    Each `Identity` has one or more resolver implementations. The default resolver
-    for AWS consults environment variables, IMDS, ~/.aws, etc.
+    Each `Identity` may have one or more resolver implementations.
     """
 
-    async def get_identity(
-        self, *, identity_properties: dict[str, Any]
-    ) -> IdentityType:
+    async def get_identity(self, *, identity_properties: dict[str, Any]) -> Identity:
         """Load the user's identity from this resolver.
 
-        :param identity_properties: Properties loaded from the service's
-        authentication rules.
-
-        :returns: The user's identity.
+        :param identity_properties: Properties used to help determine the
+        identity to return.
         """
         ...
-
-
-IdentityResolverType = TypeVar("IdentityResolverType", bound=IdentityResolver)
 
 
 class IdentityResolverConfiguration(Protocol):
     """The identity resolvers configured in the client."""
 
-    # TODO: Should we we use an IdentityType type or just a string delineating
-    # the different identity types?
-    def get_identity_resolver(
-        self, identity_type: type[IdentityType]
-    ) -> IdentityResolver:
+    def get_identity_resolver(self, identity_type: type[Identity]) -> IdentityResolver:
         """Retrieve an identity resolver for the provided identity type.
 
         :param identity_type: The type of identity to resolve.
-
-        :returns: The identity resolver for the provided identity type.
-
-        :raises `SmithyIdentityException`: If the identity type is not supported.
         """
         ...
-
-
-class HttpSigner(Protocol):
-    """An entity within the client representing a way to generate a signature for a
-    HTTP request.
-    """
-
-    def sign(
-        self,
-        http_request: Request,
-        identity: IdentityType,
-        signing_properties: dict[str, Any],
-    ) -> Request:
-        """Sign the provided HTTP request, and generate a new HTTP request with the
-        signature added.
-
-        :param http_request: The HTTP request to sign.
-
-        :param identity: The customer's identity.
-
-        :param signing_properties: Additional properties loaded from the service's
-        authentication rules.
-
-        :returns: The signed HTTP request.
-
-        :raises `SmithyIdentityException`: If the provided identity is not
-        compatible with this signer.
-        """
-        ...
-
-
-HttpSignerType = TypeVar("HttpSignerType", bound=HttpSigner)
