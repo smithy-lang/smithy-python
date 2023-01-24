6d7635b983febc7f11c514d9cb40ca48e8a4d572
Jordon Phillips <JordonPhillips@users.noreply.github.com>
2023-01-24 15:21:19 +0100

    Fix pytest-asyncio version range

--- codegen/smithy-python-codegen/src/main/java/software/amazon/smithy/python/codegen/SmithyPythonDependency.java
+++ codegen/smithy-python-codegen/src/main/java/software/amazon/smithy/python/codegen/SmithyPythonDependency.java
@@ -52,8 +52,7 @@ public final class SmithyPythonDependency {
      */
     public static final PythonDependency PYTEST_ASYNCIO = new PythonDependency(
         "pytest-asyncio",
-        ">=0.20.3,<0.30" +
-            ".0",
+        ">=0.20.3,<0.21.0",
         Type.TEST_DEPENDENCY,
         false
     );
