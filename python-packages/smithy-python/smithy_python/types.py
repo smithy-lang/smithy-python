b3b528d11235ee4f91d8b3b59cfb9e880a277b1c
Jordon Phillips <JordonPhillips@users.noreply.github.com>
2023-12-22 15:05:39 +0100

    Remove smithy-python
    This removes the smithy-python package, which has been replaced by
    smithy-core and smithy-http

--- python-packages/smithy-python/smithy_python/types.py
+++
@@ -1,6 +0,0 @@
-from collections.abc import Mapping, Sequence
-from typing import TypeAlias
-
-Document: TypeAlias = (
-    Mapping[str, "Document"] | Sequence["Document"] | str | int | float | bool | None
-)
