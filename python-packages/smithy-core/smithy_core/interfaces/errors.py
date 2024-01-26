79e6e69c56e7c49ef8e910c5620376195fade58d
Hayden Baker <haydebak@amazon.com>
2024-01-25 18:01:25 -0800

    Add error protocol

---
+++ python-packages/smithy-core/smithy_core/interfaces/errors.py
@@ -0,0 +1,12 @@
+#  Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+#  SPDX-License-Identifier: Apache-2.0
+from typing import Literal, Protocol
+
+
+class HasFault(Protocol):
+    """A protocol for a modeled error.
+    
+    All modeled errors will have a fault that is either "client" or "server".
+    """
+
+    fault: Literal["client", "server"]
