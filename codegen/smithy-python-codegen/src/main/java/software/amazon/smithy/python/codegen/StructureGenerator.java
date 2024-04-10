98218966d0a9b498c7fadf41175686905d69716f
JordonPhillips <JordonPhillips@users.noreply.github.com>
2024-04-10 13:01:23 +0200

    Use dataclasses for structures
    This updates structure / union / error generators to use dataclasses.
    All of the issues we previously had in using them have been resolved
    by updates to Python and the removal of the generated boto3 compat
    code.

--- codegen/smithy-python-codegen/src/main/java/software/amazon/smithy/python/codegen/StructureGenerator.java
+++ codegen/smithy-python-codegen/src/main/java/software/amazon/smithy/python/codegen/StructureGenerator.java
@@ -15,7 +15,6 @@
 
 package software.amazon.smithy.python.codegen;
 
-import static java.lang.String.format;
 import static software.amazon.smithy.python.codegen.CodegenUtils.isErrorMessage;
 
 import java.time.ZonedDateTime;
@@ -24,7 +23,6 @@ import java.util.List;
 import java.util.Set;
 import java.util.logging.Logger;
 import java.util.stream.Collectors;
-import java.util.stream.Stream;
 import software.amazon.smithy.codegen.core.SymbolProvider;
 import software.amazon.smithy.model.Model;
 import software.amazon.smithy.model.knowledge.NullableIndex;
@@ -35,7 +33,6 @@ import software.amazon.smithy.model.shapes.StructureShape;
 import software.amazon.smithy.model.traits.DefaultTrait;
 import software.amazon.smithy.model.traits.DocumentationTrait;
 import software.amazon.smithy.model.traits.ErrorTrait;
-import software.amazon.smithy.model.traits.RequiredTrait;
 import software.amazon.smithy.model.traits.SensitiveTrait;
 
 
@@ -97,22 +94,38 @@ final class StructureGenerator implements Runnable {
      */
     private void renderStructure() {
         writer.addStdlibImport("typing", "Dict");
-        writer.addStdlibImport("typing", "Any");
+        writer.addStdlibImport("dataclasses", "dataclass");
         var symbol = symbolProvider.toSymbol(shape);
-        writer.openBlock("class $L:", "", symbol.getName(), () -> {
-            writeProperties(false);
-            writeInit(false);
-            writeRepr(false);
-            writeEq(false);
-        });
-        writer.write("");
+
+
+        if (shape.members().isEmpty() && !shape.hasTrait(DocumentationTrait.class)) {
+            writer.write("""
+                    @dataclass
+                    class $L:
+                        pass
+
+                    """, symbol.getName());
+            return;
+        }
+
+
+
+        writer.write("""
+                @dataclass(kw_only=True)
+                class $L:
+                    ${C|}
+
+                    ${C|}
+
+                """, symbol.getName(),
+                writer.consumer(w -> writeClassDocs(false)),
+                writer.consumer(w -> writeProperties()));
     }
 
     private void renderError() {
         ErrorTrait errorTrait = shape.getTrait(ErrorTrait.class).orElseThrow(IllegalStateException::new);
-        writer.addStdlibImport("typing", "Dict");
-        writer.addStdlibImport("typing", "Any");
-        writer.addStdlibImport("typing", "Literal");
+        writer.addStdlibImports("typing", Set.of("Literal", "ClassVar"));
+        writer.addStdlibImport("dataclasses", "dataclass");
 
         // TODO: Implement protocol-level customization of the error code
         var fault = errorTrait.getValue();
@@ -121,120 +134,83 @@ final class StructureGenerator implements Runnable {
         var apiError = CodegenUtils.getApiError(settings);
 
         writer.write("""
-                class $1L($2T[Literal[$3S]]):
-                    code: Literal[$3S] = $3S
-                    fault: Literal[$4S] = $4S
-                    message: str
+                @dataclass(kw_only=True)
+                class $1L($2T):
                     ${5C|}
 
+                    code: ClassVar[Literal[$3S]] = $3S
+                    fault: ClassVar[Literal[$4S]] = $4S
+
+                    message: str
                     ${6C|}
 
-                    ${7C|}
-
-                    ${8C|}
-
                 """, symbol.getName(), apiError, code, fault,
-                writer.consumer(w -> writeProperties(true)),
-                writer.consumer(w -> writeInit(true)),
-                writer.consumer(w -> writeRepr(true)),
-                writer.consumer(w -> writeEq(true)));
+                writer.consumer(w -> writeClassDocs(true)),
+                writer.consumer(w -> writeProperties()));
     }
 
-    private void writeProperties(boolean isError) {
-        NullableIndex index = NullableIndex.of(model);
-        for (MemberShape member : shape.members()) {
-            if (isError && isErrorMessage(model, member)) {
-                continue;
+    private void writeProperties() {
+        for (MemberShape member : requiredMembers) {
+            writer.pushState();
+            writer.putContext("sensitive", false);
+            if (member.hasTrait(SensitiveTrait.class)) {
+                writer.addStdlibImport("dataclasses", "field");
+                writer.putContext("sensitive", true);
             }
+
             var memberName = symbolProvider.toMemberName(member);
-            if (index.isMemberNullable(member)) {
-                writer.addStdlibImport("typing", "Optional");
-                String formatString = format("$L: Optional[%s]", getTargetFormat(member));
-                writer.write(formatString, memberName, symbolProvider.toSymbol(member));
+            var target = model.expectShape(member.getTarget());
+            writer.putContext("quote", recursiveShapes.contains(target) ? "'" : "");
+            writer.write("""
+                    $L: ${quote:L}$T${quote:L}\
+                    ${?sensitive} = field(repr=False)${/sensitive}
+                    """,
+                    memberName, symbolProvider.toSymbol(member));
+            writer.popState();
+        }
+
+        for (MemberShape member : optionalMembers) {
+            writer.pushState();
+
+            var requiresField = false;
+            if (member.hasTrait(SensitiveTrait.class)) {
+                writer.putContext("sensitive", true);
+                writer.addStdlibImport("dataclasses", "field");
+                requiresField = true;
             } else {
-                String formatString = format("$L: %s", getTargetFormat(member));
-                writer.write(formatString, memberName, symbolProvider.toSymbol(member));
+                writer.putContext("sensitive", false);
             }
-        }
-    }
 
-    private void writeInit(boolean isError) {
-        if (!isError && shape.members().isEmpty()) {
-            writeClassDocs(false);
-            return;
-        }
-
-        var nullableIndex = NullableIndex.of(model);
-        writer.openBlock("def __init__(", "):", () -> {
-            writer.write("self,");
-            if (!shape.members().isEmpty() || isError) {
-                // Adding this star to the front prevents the use of positional arguments.
-                writer.write("*,");
-            }
-            if (isError) {
-                writer.write("message: str,");
-            }
-            for (MemberShape member : requiredMembers) {
-                var memberName = symbolProvider.toMemberName(member);
-                String formatString = format("$L: %s,", getTargetFormat(member));
-                writer.write(formatString, memberName, symbolProvider.toSymbol(member));
-            }
-            for (MemberShape member : optionalMembers) {
-                var memberName = symbolProvider.toMemberName(member);
-                if (nullableIndex.isMemberNullable(member)) {
-                    writer.addStdlibImport("typing", "Optional");
-                    String formatString = format("$L: Optional[%s] = None,", getTargetFormat(member));
-                    writer.write(formatString, memberName, symbolProvider.toSymbol(member));
-                } else if (member.hasTrait(RequiredTrait.class)) {
-                    String formatString = format("$L: %s = $L,", getTargetFormat(member));
-                    writer.write(formatString, memberName, symbolProvider.toSymbol(member),
-                            getDefaultValue(writer, member));
-                } else {
-                    // Shapes that are simple types, lists, or maps can have default values.
-                    // https://smithy.io/2.0/spec/type-refinement-traits.html#smithy-api-default-trait
-                    var target = model.expectShape(member.getTarget());
-                    var memberSymbol = symbolProvider.toSymbol(member);
-                    String formatString;
-                    if (target.isDocumentShape() || target.isListShape() || target.isMapShape()) {
-                        // Documents, lists, and maps can have mutable defaults so just use None in the
-                        // constructor.
-                        writer.addStdlibImport("typing", "Optional");
-                        formatString = format("$L: Optional[%1$s] = None,", getTargetFormat(member));
-                        writer.write(formatString, memberName, memberSymbol);
-                    } else {
-                        formatString = format("$L: %s = $L,", getTargetFormat(member));
-                        writer.write(formatString, memberName, memberSymbol, getDefaultValue(writer, member));
-                    }
+            var defaultValue = "None";
+            var defaultKey = "default";
+            if (member.hasTrait(DefaultTrait.class)) {
+                defaultValue = getDefaultValue(writer, member);
+                if (Set.of("list", "dict").contains(defaultValue)) {
+                    writer.addStdlibImport("dataclasses", "field");
+                    defaultKey = "default_factory";
+                    requiresField = true;
                 }
-            }
-        });
 
-        writer.indent();
-
-        writeClassDocs(isError);
-        if (isError) {
-            writer.write("super().__init__(message)");
-        }
-
-        Stream.concat(requiredMembers.stream(), optionalMembers.stream()).forEach(member -> {
-            String memberName = symbolProvider.toMemberName(member);
-            if (isOptionalDefault(member)) {
-                writer.write("self.$1L = $1L if $1L is not None else $2L",
-                    memberName, getDefaultValue(writer, member));
             } else {
-                writer.write("self.$1L = $1L", memberName);
+                writer.putContext("nullable", true);
             }
-        });
-        writer.dedent();
-        writer.write("");
-    }
+            writer.putContext("defaultKey", defaultKey);
+            writer.putContext("defaultValue", defaultValue);
+            writer.putContext("useField", requiresField);
 
-    private boolean isOptionalDefault(MemberShape member) {
-        // If a member with a default value isn't required, it's optional.
-        // see: https://smithy.io/2.0/spec/type-refinement-traits.html#smithy-api-default-trait
-        var target = model.expectShape(member.getTarget());
-        return member.hasTrait(DefaultTrait.class) && !member.hasTrait(RequiredTrait.class)
-            && (target.isDocumentShape() || target.isListShape() || target.isMapShape());
+            var target = model.expectShape(member.getTarget());
+            writer.putContext("quote", recursiveShapes.contains(target) ? "'" : "");
+
+            var memberName = symbolProvider.toMemberName(member);
+            writer.write("""
+                    $L: ${quote:L}$T${?nullable} | None${/nullable}${quote:L} \
+                    = ${^useField}${defaultValue:L}${/useField}\
+                    ${?useField}\
+                    field(${?sensitive}repr=False, ${/sensitive}${defaultKey:L}=${defaultValue:L})\
+                    ${/useField}""", memberName, symbolProvider.toSymbol(member));
+
+            writer.popState();
+        }
     }
 
     private void writeClassDocs(boolean isError) {
@@ -269,17 +245,6 @@ final class StructureGenerator implements Runnable {
                 .collect(Collectors.toList());
     }
 
-    private String getTargetFormat(MemberShape member) {
-        Shape target = model.expectShape(member.getTarget());
-        // Recursive shapes may be referenced before they're defined even with
-        // a topological sort. So forward references need to be used when
-        // referencing them.
-        if (recursiveShapes.contains(target)) {
-            return "'$T'";
-        }
-        return "$T";
-    }
-
     private String getDefaultValue(PythonWriter writer, MemberShape member) {
         // The default value is defined in the model is a exposed as generic
         // json, so we need to convert it to the proper type based on the target.
@@ -295,6 +260,10 @@ final class StructureGenerator implements Runnable {
         return switch (defaultNode.getType()) {
             case NULL -> "None";
             case BOOLEAN -> defaultNode.expectBooleanNode().getValue() ? "True" : "False";
+            // These will be given to a default_factory in field. They're inherently empty, so no need to
+            // worry about any potential values.
+            case ARRAY -> "list";
+            case OBJECT -> "dict";
             default -> Node.printJson(defaultNode);
         };
     }
@@ -317,74 +286,5 @@ final class StructureGenerator implements Runnable {
             String docs = writer.formatDocs(String.format(":param %s: %s", memberName, trait.getValue()));
             writer.write(docs);
         });
-    }
-
-    private void writeRepr(boolean isError) {
-        var symbol = symbolProvider.toSymbol(shape);
-        writer.write("""
-            def __repr__(self) -> str:
-                result = "$L("
-                ${C|}
-                return result + ")"
-            """, symbol.getName(), (Runnable) () -> writeReprMembers(isError));
-    }
-
-    private void writeReprMembers(boolean isError) {
-        if (isError) {
-            writer.write("result += f'message={self.message},'");
-        }
-        var iter = shape.members().iterator();
-        while (iter.hasNext()) {
-            var member = iter.next();
-            var memberName = symbolProvider.toMemberName(member);
-            var trailingComma = iter.hasNext() ? ", " : "";
-            if (member.hasTrait(SensitiveTrait.class)) {
-                // Sensitive members must not be printed
-                // see: https://smithy.io/2.0/spec/documentation-traits.html#smithy-api-sensitive-trait
-                writer.write("""
-                    if self.$1L is not None:
-                        result += f"$1L=...$2L"
-                    """, memberName, trailingComma);
-            } else {
-                writer.write("""
-                    if self.$1L is not None:
-                        result += f"$1L={repr(self.$1L)}$2L"
-                    """, memberName, trailingComma);
-            }
-        }
-    }
-
-    private void writeEq(boolean isError) {
-        var symbol = symbolProvider.toSymbol(shape);
-        writer.addStdlibImport("typing", "Any");
-        var attributeList = new StringBuilder("[");
-        if (isError) {
-            attributeList.append("'message',");
-        }
-        for (MemberShape member : shape.members()) {
-            attributeList.append(String.format("'%s',", symbolProvider.toMemberName(member)));
-        }
-        attributeList.append("]");
-
-        if (!isError && shape.members().isEmpty()) {
-            writer.write("""
-                def __eq__(self, other: Any) -> bool:
-                    return isinstance(other, $L)
-                """, symbol.getName());
-            return;
-        }
-
-        // Use a generator expression inside "all" here to save some space while still
-        // lazily evaluating each equality check.
-        writer.write("""
-            def __eq__(self, other: Any) -> bool:
-                if not isinstance(other, $L):
-                    return False
-                attributes: list[str] = $L
-                return all(
-                    getattr(self, a) == getattr(other, a)
-                    for a in attributes
-                )
-            """, symbol.getName(), attributeList.toString());
     }
 }
