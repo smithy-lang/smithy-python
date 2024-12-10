3f372f6ade000aad644851c32b4b87e95d4848d9
JordonPhillips <JordonPhillips@users.noreply.github.com>
2024-12-10 14:53:59 +0100

    Raise modeled errors
    Modeled errors should be raised, this updates the event receiver to
    raise them.

--- python-packages/aws-event-stream/tests/unit/_private/test_deserializers.py
+++ python-packages/aws-event-stream/tests/unit/_private/test_deserializers.py
@@ -1,12 +1,17 @@
 # Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
 # SPDX-License-Identifier: Apache-2.0
 from io import BytesIO
+from typing import Any
 
 import pytest
+from smithy_core.aio.types import AsyncBytesReader
 from smithy_core.deserializers import DeserializeableShape
 from smithy_json import JSONCodec
 
-from aws_event_stream._private.deserializers import EventDeserializer
+from aws_event_stream._private.deserializers import (
+    AWSAsyncEventReceiver,
+    EventDeserializer,
+)
 from aws_event_stream.events import Event, EventMessage
 from aws_event_stream.exceptions import UnmodeledEventError
 
@@ -14,9 +19,33 @@ from . import (
     EVENT_STREAM_SERDE_CASES,
     INITIAL_REQUEST_CASE,
     INITIAL_RESPONSE_CASE,
+    ErrorEvent,
     EventStreamDeserializer,
+    EventStreamErrorEvent,
     EventStreamOperationInputOutput,
 )
+
+
+@pytest.mark.parametrize("expected,given", EVENT_STREAM_SERDE_CASES)
+async def test_event_receiver(expected: DeserializeableShape, given: EventMessage):
+    source = AsyncBytesReader(given.encode())
+    deserializer = EventStreamDeserializer()
+    receiver = AWSAsyncEventReceiver[Any](
+        payload_codec=JSONCodec(), source=source, deserializer=deserializer.deserialize
+    )
+
+    result: Any = None
+
+    try:
+        result = await receiver.receive()
+    except ErrorEvent as e:
+        if isinstance(expected, EventStreamErrorEvent):
+            expected = expected.value
+        else:
+            raise
+        result = e
+
+    assert result == expected
 
 
 @pytest.mark.parametrize("expected,given", EVENT_STREAM_SERDE_CASES)
