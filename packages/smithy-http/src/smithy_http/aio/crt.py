eeeafe9f494fbc65b06004e607011b462020e393
Alex Woods <alextwoods@outlook.com>
2025-03-18 13:18:48 -0700

    Update packages/smithy-http/src/smithy_http/aio/crt.py
    Co-authored-by: Nate Prewitt <nate.prewitt@gmail.com>

--- packages/smithy-http/src/smithy_http/aio/crt.py
+++ packages/smithy-http/src/smithy_http/aio/crt.py
@@ -349,7 +349,6 @@ class AWSCRTHTTPClient(http_aio_interfaces.HTTPClient):
                 headers_list.append((fld.name, val))
 
         path = self._render_path(request.destination)
-        print("CRT path: ", path)
         headers = crt_http.HttpHeaders(headers_list)
 
         body = request.body
