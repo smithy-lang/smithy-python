2cd56254ac50bdd814d0a8cb135e59052b77d039
Alex Woods <alexwoo@amazon.com>
2025-03-18 14:12:58 -0700

    Implement a configurable credentials resolver chain

--- packages/smithy-aws-core/src/smithy_aws_core/credentials_resolvers/__init__.py
+++ packages/smithy-aws-core/src/smithy_aws_core/credentials_resolvers/__init__.py
@@ -1,6 +1,12 @@
 #  Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
 #  SPDX-License-Identifier: Apache-2.0
+
 from .environment import EnvironmentCredentialsResolver
 from .static import StaticCredentialsResolver
+from .credentials_resolver_chain import CredentialsResolverChain
 
-__all__ = ("EnvironmentCredentialsResolver", "StaticCredentialsResolver")
+__all__ = (
+    "EnvironmentCredentialsResolver",
+    "StaticCredentialsResolver",
+    "CredentialsResolverChain",
+)
---
+++ packages/smithy-aws-core/src/smithy_aws_core/credentials_resolvers/credentials_resolver_chain.py
@@ -0,0 +1,54 @@
+from typing import Callable, List
+
+from smithy_aws_core.credentials_resolvers import EnvironmentCredentialsResolver
+from smithy_aws_core.identity import AWSCredentialsIdentity, AWSCredentialsResolver
+from smithy_core.aio.interfaces.identity import IdentityResolver
+from smithy_core.exceptions import SmithyIdentityException
+from smithy_core.interfaces.identity import IdentityProperties
+
+import os
+
+
+def _env_creds_available() -> bool:
+    return bool(os.getenv("AWS_ACCESS_KEY_ID")) and bool(
+        os.getenv("AWS_SECRET_ACCESS_KEY")
+    )
+
+
+def _build_env_creds() -> AWSCredentialsResolver:
+    return EnvironmentCredentialsResolver()
+
+
+type CredentialSource = tuple[Callable[[], bool], Callable[[], AWSCredentialsResolver]]
+_DEFAULT_SOURCES: list[CredentialSource] = [(_env_creds_available, _build_env_creds)]
+
+
+class CredentialsResolverChain(
+    IdentityResolver[AWSCredentialsIdentity, IdentityProperties]
+):
+    """Resolves AWS Credentials from system environment variables."""
+
+    def __init__(self, *, sources: List[CredentialSource] | None = None):
+        if sources is None:
+            sources = _DEFAULT_SOURCES
+        self._sources: List[CredentialSource] = sources
+        self._credentials_resolver: AWSCredentialsResolver | None = None
+
+    async def get_identity(
+        self, *, identity_properties: IdentityProperties
+    ) -> AWSCredentialsIdentity:
+        if self._credentials_resolver is not None:
+            return await self._credentials_resolver.get_identity(
+                identity_properties=identity_properties
+            )
+
+        for source in self._sources:
+            if source[0]():
+                self._credentials_resolver = source[1]()
+                return await self._credentials_resolver.get_identity(
+                    identity_properties=identity_properties
+                )
+
+        raise SmithyIdentityException(
+            "None of the configured credentials sources were able to resolve credentials."
+        )
