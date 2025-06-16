c520e31392963bd470b9e6a04ee28553707babd9
Nate Prewitt <nate.prewitt@gmail.com>
2025-06-16 16:27:37 -0600

    Add top-level import for ContainerCredentialResolver

--- packages/smithy-aws-core/src/smithy_aws_core/credentials_resolvers/__init__.py
+++ packages/smithy-aws-core/src/smithy_aws_core/credentials_resolvers/__init__.py
@@ -1,10 +1,12 @@
 #  Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
 #  SPDX-License-Identifier: Apache-2.0
+from .container import ContainerCredentialResolver
 from .environment import EnvironmentCredentialsResolver
 from .imds import IMDSCredentialsResolver
 from .static import StaticCredentialsResolver
 
 __all__ = (
+    "ContainerCredentialResolver",
     "EnvironmentCredentialsResolver",
     "IMDSCredentialsResolver",
     "StaticCredentialsResolver",
