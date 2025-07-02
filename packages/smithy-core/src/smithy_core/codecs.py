98d32aeee60d96de6236dafd70c9031bf1f558a0
Nate Prewitt <nate.prewitt@gmail.com>
2025-07-02 11:50:53 -0600

    Add Apache-2.0 headers to smithy-core

--- packages/smithy-core/src/smithy_core/codecs.py
+++ packages/smithy-core/src/smithy_core/codecs.py
@@ -1,3 +1,5 @@
+# Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+# SPDX-License-Identifier: Apache-2.0
 from io import BytesIO
 from typing import TYPE_CHECKING, Protocol, runtime_checkable
 
--- packages/smithy-core/src/smithy_core/shapes.py
+++ packages/smithy-core/src/smithy_core/shapes.py
@@ -1,3 +1,5 @@
+# Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+# SPDX-License-Identifier: Apache-2.0
 from enum import Enum
 from typing import Self
 
