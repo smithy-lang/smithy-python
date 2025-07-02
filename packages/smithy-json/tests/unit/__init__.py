ad576db18b030284c807c07fcf3b6e4cf32442fb
Nate Prewitt <nate.prewitt@gmail.com>
2025-07-02 12:33:25 -0600

    Add Apache-2.0 headers to smithy-json

--- packages/smithy-json/tests/unit/__init__.py
+++ packages/smithy-json/tests/unit/__init__.py
@@ -1,3 +1,5 @@
+# Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+# SPDX-License-Identifier: Apache-2.0
 from dataclasses import dataclass
 from datetime import UTC, datetime
 from decimal import Decimal
