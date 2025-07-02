36aea1abd875ff97ca30992922c3917a25a8dcc9
Nate Prewitt <nate.prewitt@gmail.com>
2025-07-02 12:02:12 -0600

    Add Apache-2.0 headers to smithy-http

--- packages/smithy-http/src/smithy_http/aio/protocols.py
+++ packages/smithy-http/src/smithy_http/aio/protocols.py
@@ -1,3 +1,5 @@
+# Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+# SPDX-License-Identifier: Apache-2.0
 import os
 from collections.abc import AsyncIterable
 from inspect import iscoroutinefunction
