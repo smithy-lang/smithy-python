593ba48fad4d06ab24595a716b29bc8ad66a7c18
Nate Prewitt <nate.prewitt@gmail.com>
2025-07-02 11:58:41 -0600

    Add Apache-2.0 headers to smithy-aws-event-stream

--- packages/smithy-aws-event-stream/src/smithy_aws_event_stream/exceptions.py
+++ packages/smithy-aws-event-stream/src/smithy_aws_event_stream/exceptions.py
@@ -1,3 +1,5 @@
+# Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+# SPDX-License-Identifier: Apache-2.0
 """Binary Event Stream support for the application/vnd.amazon.eventstream format."""
 
 from typing import Any
