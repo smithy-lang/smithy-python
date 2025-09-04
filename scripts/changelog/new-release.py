23bbbc145be4ad1a4f3f303e808ffc321935a74e
jonathan343 <jonathangee09@gmail.com>
2025-09-04 13:57:11 -0400

    Add license header to scripts

--- scripts/changelog/new-release.py
+++ scripts/changelog/new-release.py
@@ -1,4 +1,6 @@
 #!/usr/bin/env python3
+# Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+# SPDX-License-Identifier: Apache-2.0
 """
 Create a new release by consolidating changelog entries from next-release directory
 into a version JSON file (x.y.z.json).
