1addab832924f132c49f61fe1b3eb8b6fa42b061
SamRemis <sjremis94@gmail.com>
2025-11-21 10:29:56 -0500

    Prepare release

--- packages/smithy-http/src/smithy_http/__init__.py
+++ packages/smithy-http/src/smithy_http/__init__.py
@@ -6,7 +6,7 @@ from collections.abc import Iterable, Iterator
 from . import interfaces
 from .interfaces import FieldPosition
 
-__version__ = "0.2.1"
+__version__ = "0.3.0"
 
 
 class Field(interfaces.Field):
