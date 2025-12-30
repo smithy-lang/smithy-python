95e604084c99e52ff7a7ee3e26f6167e0e36d55d
Nate Prewitt <nate.prewitt@gmail.com>
2025-12-30 12:01:48 -0700

    Release: smithy-python codegen 0.2.0

--- codegen/core/src/main/java/software/amazon/smithy/python/codegen/SmithyPythonDependency.java
+++ codegen/core/src/main/java/software/amazon/smithy/python/codegen/SmithyPythonDependency.java
@@ -22,7 +22,7 @@ public final class SmithyPythonDependency {
      */
     public static final PythonDependency SMITHY_CORE = new PythonDependency(
             "smithy_core",
-            "~=0.2.0",
+            "~=0.3.0",
             Type.DEPENDENCY,
             false);
 
@@ -78,7 +78,7 @@ public final class SmithyPythonDependency {
      */
     public static final PythonDependency SMITHY_AWS_CORE = new PythonDependency(
             "smithy_aws_core",
-            "~=0.2.0",
+            "~=0.3.0",
             Type.DEPENDENCY,
             false);
 
