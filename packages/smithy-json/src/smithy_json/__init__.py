0126c025b764eb174c1476cb5ea79d674d098692
Nate Prewitt <nate.prewitt@gmail.com>
2025-12-30 11:16:55 -0700

    Release: smithy_json-0.2.1

--- packages/smithy-json/src/smithy_json/__init__.py
+++ packages/smithy-json/src/smithy_json/__init__.py
@@ -13,7 +13,7 @@ from ._private.documents import JSONDocument
 from ._private.serializers import JSONShapeSerializer as _JSONShapeSerializer
 from .settings import JSONSettings
 
-__version__ = "0.2.0"
+__version__ = "0.2.1"
 __all__ = ("JSONCodec", "JSONDocument", "JSONSettings")
 
 
