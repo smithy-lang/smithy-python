a0f328f64ce76ce1765b6257e628b0e674a54d61
ubaskota <19787410+ubaskota@users.noreply.github.com>
2026-03-12 19:58:50 -0400

    fix format

--- packages/smithy-aws-core/src/smithy_aws_core/config/source_info.py
+++ packages/smithy-aws-core/src/smithy_aws_core/config/source_info.py
@@ -23,6 +23,7 @@ class SimpleSource:
 
     Examples: region from environment, max_attempts from config file.
     """
+
     # TODO: Currently only environment variable is implemented as a config
     # source. Tests use raw strings (e.g., "environment", "config_file") as
     # source names to simulate multi-source scenarios. Once additional
