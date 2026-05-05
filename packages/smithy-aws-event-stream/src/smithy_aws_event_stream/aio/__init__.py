abc1a005090d29be63f14f15e84aa0f05ba982ed
Alessandra Romero <24320222+alexgromero@users.noreply.github.com>
2026-05-05 13:52:42 -0400

    Fix missing signed empty end frame on event stream close (#691)
    * Send signed empty end frame on event stream close
    
    * Update signer to use sign_empty method
    
    * Add sign_empty method to EventSigner protocol
    
    * Update unit test
    
    * Address PR feedback
    
    * Update changelog entries to feature type

--- packages/smithy-aws-event-stream/src/smithy_aws_event_stream/aio/__init__.py
+++ packages/smithy-aws-event-stream/src/smithy_aws_event_stream/aio/__init__.py
@@ -17,7 +17,7 @@ from smithy_core.serializers import SerializeableShape
 
 from .._private.deserializers import EventDeserializer as _EventDeserializer
 from .._private.serializers import EventSerializer as _EventSerializer
-from ..events import Event
+from ..events import Event, EventMessage
 from ..exceptions import EventError
 
 logger = logging.getLogger(__name__)
@@ -85,9 +85,26 @@ class AWSEventPublisher[E: SerializeableShape](EventPublisher[E]):
             return
         self._closed = True
 
-        if (close := getattr(self._writer, "close", None)) is not None:
-            if asyncio.iscoroutine(result := close()):
-                await result
+        try:
+            # Send a signed empty frame to signal stream completion.
+            if self._signing_config is not None:
+                end_frame = EventMessage()
+                identity = await self._signing_config.identity_resolver.get_identity(
+                    properties=self._signing_config.identity_properties
+                )
+                end_frame = await self._signing_config.signer.sign_empty(
+                    event=end_frame,
+                    identity=identity,
+                    properties=self._signing_config.signing_properties,
+                )
+                logger.debug(
+                    "Sending signed empty message to terminate the event stream."
+                )
+                await self._writer.write(end_frame.encode())
+        finally:
+            if (close := getattr(self._writer, "close", None)) is not None:
+                if asyncio.iscoroutine(result := close()):
+                    await result
 
     @property
     def closed(self) -> bool:
