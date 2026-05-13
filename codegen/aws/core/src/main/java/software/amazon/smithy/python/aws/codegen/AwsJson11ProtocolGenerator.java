7bd7e6215541b9e749e98c8181f11de94f49ab56
jonathan343 <jonathangee09@gmail.com>
2026-05-12 23:25:40 -0400

    codegen: add AWS JSON protocol generators

---
+++ codegen/aws/core/src/main/java/software/amazon/smithy/python/aws/codegen/AwsJson11ProtocolGenerator.java
@@ -0,0 +1,71 @@
+/*
+ * Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ * SPDX-License-Identifier: Apache-2.0
+ */
+package software.amazon.smithy.python.aws.codegen;
+
+import java.util.Set;
+import software.amazon.smithy.aws.traits.protocols.AwsJson1_1Trait;
+import software.amazon.smithy.model.node.ArrayNode;
+import software.amazon.smithy.model.node.ObjectNode;
+import software.amazon.smithy.model.shapes.ShapeId;
+import software.amazon.smithy.python.codegen.ApplicationProtocol;
+import software.amazon.smithy.python.codegen.GenerationContext;
+import software.amazon.smithy.python.codegen.HttpProtocolTestGenerator;
+import software.amazon.smithy.python.codegen.SymbolProperties;
+import software.amazon.smithy.python.codegen.generators.ProtocolGenerator;
+import software.amazon.smithy.python.codegen.writer.PythonWriter;
+import software.amazon.smithy.utils.SmithyInternalApi;
+
+@SmithyInternalApi
+public final class AwsJson11ProtocolGenerator implements ProtocolGenerator {
+    private static final Set<String> TESTS_TO_SKIP = Set.of(
+            // These tests essentially try to assert nan == nan, which is never true.
+            // The generator needs protocol-specific assertions before enabling them.
+            "AwsJson11SupportsNaNFloatInputs",
+
+            // TODO: support the request compression trait.
+            "SDKAppliedContentEncoding_awsJson1_1",
+            "SDKAppendsGzipAndIgnoresHttpProvidedEncoding_awsJson1_1",
+
+            // TODO: support the endpoint trait.
+            "AwsJson11EndpointTrait",
+            "AwsJson11EndpointTraitWithHostLabel");
+
+    @Override
+    public ShapeId getProtocol() {
+        return AwsJson1_1Trait.ID;
+    }
+
+    @Override
+    public ApplicationProtocol getApplicationProtocol(GenerationContext context) {
+        var service = context.settings().service(context.model());
+        var trait = service.expectTrait(AwsJson1_1Trait.class);
+        var config = ObjectNode.builder()
+                .withMember("http", ArrayNode.fromStrings(trait.getHttp()))
+                .withMember("eventStreamHttp", ArrayNode.fromStrings(trait.getEventStreamHttp()))
+                .build();
+        return ApplicationProtocol.createDefaultHttpApplicationProtocol(config);
+    }
+
+    @Override
+    public void initializeProtocol(GenerationContext context, PythonWriter writer) {
+        writer.addDependency(AwsPythonDependency.SMITHY_AWS_CORE.withOptionalDependencies("json"));
+        writer.addImport("smithy_aws_core.aio.protocols", "AwsJson11ClientProtocol");
+        var serviceSymbol = context.symbolProvider().toSymbol(context.settings().service(context.model()));
+        var serviceSchema = serviceSymbol.expectProperty(SymbolProperties.SCHEMA);
+        writer.write("AwsJson11ClientProtocol($T)", serviceSchema);
+    }
+
+    @Override
+    public void generateProtocolTests(GenerationContext context) {
+        context.writerDelegator()
+                .useFileWriter("./tests/test_awsjson11_protocol.py", "tests.test_awsjson11_protocol", writer -> {
+                    new HttpProtocolTestGenerator(
+                            context,
+                            getProtocol(),
+                            writer,
+                            (shape, testCase) -> TESTS_TO_SKIP.contains(testCase.getId())).run();
+                });
+    }
+}
